d7ffa5da712ff28322cbedce91c105b7943ba7d5
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2023-03-30 11:30:44 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.scss';
+
+function App() {
+  return <div className="start">Start</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from 'components/App';
 import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/ex">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
